fb3f3173be66bb38e0730c714cc58d5d6cf78329
sanditor <sluque@misena.edu.co>
2021-04-24 17:53:57 -0500

    Actualización BDDD

--- DB/ticket.sql
+++ DB/ticket.sql
@@ -1,126 +1,81 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 06-01-2018 a las 01:53:45
--- Versión del servidor: 10.1.24-MariaDB
--- Versión de PHP: 7.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `ticket`
---
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         10.4.10-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             11.2.0.6213
 -- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `administrador`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE `administrador` (
-  `id_admin` int(11) NOT NULL,
+
+-- Volcando estructura de base de datos para ticket
+CREATE DATABASE IF NOT EXISTS `ticket` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `ticket`;
+
+-- Volcando estructura para tabla ticket.administrador
+CREATE TABLE IF NOT EXISTS `administrador` (
+  `id_admin` int(11) NOT NULL AUTO_INCREMENT,
   `nombre_completo` varchar(100) NOT NULL,
   `nombre_admin` varchar(60) NOT NULL,
   `clave` text NOT NULL,
-  `email_admin` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `email_admin` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_admin`),
+  UNIQUE KEY `correo` (`email_admin`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
---
--- Volcado de datos para la tabla `administrador`
---
-
+-- Volcando datos para la tabla ticket.administrador: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `administrador` DISABLE KEYS */;
 INSERT INTO `administrador` (`id_admin`, `nombre_completo`, `nombre_admin`, `clave`, `email_admin`) VALUES
-(1, 'Super Administrador', 'Administrador', '2a2e9a58102784ca18e2605a4e727b5f', 'administrador@gmail.com');
+	(1, 'Super Administrador', 'Administrador', '2a2e9a58102784ca18e2605a4e727b5f', 'administrador@gmail.com');
+/*!40000 ALTER TABLE `administrador` ENABLE KEYS */;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cliente`
---
-
-CREATE TABLE `cliente` (
-  `id_cliente` int(11) NOT NULL,
+-- Volcando estructura para tabla ticket.cliente
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
   `nombre_completo` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
   `nombre_usuario` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
   `email_cliente` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
-  `clave` text COLLATE utf8_spanish2_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+  `clave` text COLLATE utf8_spanish2_ci NOT NULL,
+  PRIMARY KEY (`id_cliente`),
+  UNIQUE KEY `id_num` (`email_cliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
--- --------------------------------------------------------
+-- Volcando datos para la tabla ticket.cliente: ~2 rows (aproximadamente)
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` (`id_cliente`, `nombre_completo`, `nombre_usuario`, `email_cliente`, `clave`) VALUES
+	(1, 'pepe mojica', 'mojica', 'mojica@mojica.com', 'e10adc3949ba59abbe56e057f20f883e'),
+	(2, 'Usuario', 'usuario', 'usuario@usuario.com', 'a5ae0861febff1aeefb6d5b759d904a6');
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
 
---
--- Estructura de tabla para la tabla `ticket`
---
-
-CREATE TABLE `ticket` (
-  `id` int(11) NOT NULL,
-  `fecha` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
+-- Volcando estructura para tabla ticket.ticket
+CREATE TABLE IF NOT EXISTS `ticket` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha_creacion_PQR` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
+  `fecha_limite_atencion` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
   `serie` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
-  `estado_ticket` varchar(60) COLLATE utf8_spanish2_ci NOT NULL,
   `nombre_usuario` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
-  `email_cliente` varchar(60) COLLATE utf8_spanish2_ci NOT NULL,
-  `departamento` varchar(70) COLLATE utf8_spanish2_ci NOT NULL,
-  `asunto` varchar(70) COLLATE utf8_spanish2_ci NOT NULL,
-  `mensaje` text COLLATE utf8_spanish2_ci NOT NULL,
-  `solucion` text COLLATE utf8_spanish2_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+  `email_usuario` varchar(60) COLLATE utf8_spanish2_ci NOT NULL,
+  `Tipo_pqr` varchar(70) COLLATE utf8_spanish2_ci NOT NULL,
+  `asunto_pqr` varchar(70) COLLATE utf8_spanish2_ci NOT NULL,
+  `estado_pqr` varchar(60) COLLATE utf8_spanish2_ci NOT NULL,
+  `descripcion_pqr` text COLLATE utf8_spanish2_ci NOT NULL,
+  `solucion` text COLLATE utf8_spanish2_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `serie` (`serie`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
---
--- Índices para tablas volcadas
---
+-- Volcando datos para la tabla ticket.ticket: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
+INSERT INTO `ticket` (`id`, `fecha_creacion_PQR`, `fecha_limite_atencion`, `serie`, `nombre_usuario`, `email_usuario`, `Tipo_pqr`, `asunto_pqr`, `estado_pqr`, `descripcion_pqr`, `solucion`) VALUES
+	(3, '24/04/2021', '27/04/2021', 'PQRN1', 'usuario', 'usuario@usuario.com', 'Peticion', 'Revisión Equipo', 'Nuevo', 'Equipo dañado', 'En tres días se da respuesta');
+/*!40000 ALTER TABLE `ticket` ENABLE KEYS */;
 
---
--- Indices de la tabla `administrador`
---
-ALTER TABLE `administrador`
-  ADD PRIMARY KEY (`id_admin`),
-  ADD UNIQUE KEY `correo` (`email_admin`);
-
---
--- Indices de la tabla `cliente`
---
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`id_cliente`),
-  ADD UNIQUE KEY `id_num` (`email_cliente`);
-
---
--- Indices de la tabla `ticket`
---
-ALTER TABLE `ticket`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `serie` (`serie`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `administrador`
---
-ALTER TABLE `administrador`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT de la tabla `cliente`
---
-ALTER TABLE `cliente`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `ticket`
---
-ALTER TABLE `ticket`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
-
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
